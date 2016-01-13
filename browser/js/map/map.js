2e6eb65bbe7991d080947b3ccd6764bf1d419ea7
TheDooner64 <Robert.Muldoon.1@gmail.com>
2016-01-13 18:55:48 -0500

    Add click event to map markers

--- browser/js/map/map.js
+++ browser/js/map/map.js
@@ -4,9 +4,9 @@ app.config(function($stateProvider) {
         url: '/map',
         templateUrl: 'js/map/map.html',
         controller: 'MapCtrl',
-        resolve:{
-            apartments: function(ApartmentFactory){
-                return ApartmentFactory.getAllApartments()
+        resolve: {
+            apartments: function(ApartmentFactory) {
+                return ApartmentFactory.getAllApartments();
             }
         }
     });
@@ -15,7 +15,7 @@ app.config(function($stateProvider) {
 
 app.controller('MapCtrl', function($scope, MapFactory, FilterFactory, apartments) {
     $scope.map = MapFactory.initialize_gmaps();
-    $scope.apartments=apartments;
+    $scope.apartments = apartments;
 
     // Change bedroom options to numbers so they match database
     // Need to figure out how to display 0 as "studio" on front end, and handle the 3+
@@ -39,26 +39,36 @@ app.controller('MapCtrl', function($scope, MapFactory, FilterFactory, apartments
     $scope.ratingOptions = [1, 2, 3, 4, 5];
     $scope.termOfLease = ["1 month", "3 months", "6 months", "1 year", "2 years"];
 
+    // Default to false so the side-panel is not displayed
+    $scope.apartmentIsSelected = true;
+
     // Place to store all of the currentMarkers, in case we need it
     $scope.currentMarkers = [];
+
+    $scope.selectApartment = function() {
+
+    }
 
     // Function to add the markers to the map
     var addMarkersToMap = function(apartments) {
         apartments.forEach(function(apartment) {
-            if (apartment.latLong){
+            if (apartment.latLong) {
                 var createdMapMarker = MapFactory.drawLocation($scope.map, apartment, {
                     icon: "/assets/images/home.png"
                 });
                 createdMapMarker["apartmentId"] = apartment._id;
+
+                // Add click event to marker
+                createdMapMarker.addListener("click", $scope.selectApartment);
+
                 $scope.currentMarkers.push(createdMapMarker);
             }
         });
-        console.log($scope.currentMarkers)
     }
-    var removeHalf = function(){
-        for (var i = 0; i< $scope.currentMarkers.length; i++){
-            if(i< $scope.currentMarkers.length/2){
-                console.log("hi")
+
+    var removeHalf = function() {
+        for (var i = 0; i < $scope.currentMarkers.length; i++) {
+            if (i < $scope.currentMarkers.length / 2) {
                 $scope.currentMarkers[i].setMap(null);
             }
         }
@@ -68,20 +78,18 @@ app.controller('MapCtrl', function($scope, MapFactory, FilterFactory, apartments
         FilterFactory.filterResults($scope.filterCriteria)
             .then(function(apartments) {
                 console.log("Apartments found: ", apartments);
-                var filteredIds = apartments.map(function(apartment){
+                var filteredIds = apartments.map(function(apartment) {
                     return apartment._id;
                 });
-                $scope.currentMarkers.forEach(function(marker){
-                    if(filteredIds.indexOf(marker["apartmentId"]) === -1 ){
+                $scope.currentMarkers.forEach(function(marker) {
+                    if (filteredIds.indexOf(marker["apartmentId"]) === -1) {
                         marker.setMap(null);
                     }
                 })
             });
     }
 
-// neighborhoodQuery = "https://maps.googleapis.com/maps/api/geocode/json?address=" +city + state + "&components=locality:" +neighborhood + "&key= + apiKey"
-
-    addMarkersToMap($scope.apartments)
+    addMarkersToMap($scope.apartments);
     // Function to retrieve apartments based on user filters
 
     // This is just for testing the child state, delete later
