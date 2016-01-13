2744b736b4dc6dc1bca4f019199c3570cdc4fc52
Michael DeNatale <mikedeeno84@gmail.com>
2016-01-13 17:21:07 -0500

    add markers based on incoming apartments

--- server/app/routes/apartments/index.js
+++ server/app/routes/apartments/index.js
@@ -9,6 +9,17 @@ var bodyParser = require('body-parser');
 
 // Retrieving apartments based on criteria, which are sent in the req.body
 // POST /api/apartments/filter
+
+router.get('/', function(req, res, next){
+    Apartment.find({availability:"available"}).exec().
+    then(function(apartments){
+        res.json(apartments);
+    }).then(null, function(err){
+        throw new Error("Something went wrong when finding apartments!");
+        next(err);
+    })
+})
+
 router.post('/filter', function(req, res, next) {
 
     // NOTE: Need to cleanse inputs for the database search
