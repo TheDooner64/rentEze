ccc6d09f246798d2bc838b94a6ea0e79c7b1bd19
Michael DeNatale <mikedeeno84@gmail.com>
2016-01-14 13:13:08 -0500

    work on state change

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,6 +1,18 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html'
+        templateUrl: 'js/home/home.html',
+        controller: function ($scope, $state, neighborhoods){
+            $scope.neighborhoods = neighborhoods;
+            console.log($scope.neighborhoods)
+            $scope.search = function (){
+                $state.go('map', {query:$scope.query})
+            }
+        },
+        resolve: {
+            neighborhoods: function(NeighborhoodFactory){
+                return NeighborhoodFactory.getNeighborhoods();
+            }
+        }
     });
 });
