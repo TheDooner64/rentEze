abf98cd29756f9fef898340530743c6760bd9212
Lily Jen <lily@lilyjen.com>
2016-01-14 17:04:36 -0500

    Some styling on the navbar (logout button, ugly favorites link) and localStorage test. npm install '

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,6 +1,28 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html'
+        templateUrl: 'js/home/home.html',
+        controller: 'HomeCtrl'
     });
+});
+
+app.controller('HomeCtrl', function($scope, localStorageService) {
+    var fakeApt = {
+        title: "Awesome Apartment",
+        landlord: "Bobby McD",
+        numBedrooms: 5,
+        numBathrooms: 3
+    };
+
+    localStorageService.set('apartment', fakeApt);
+
+    $scope.showStorage = function() {
+        console.log("Here's the storage");
+        var value = localStorageService.get('apartment');
+        console.log(value);
+        localStorageService.remove('apartment');
+        console.log("Ok, now I cleared it");
+        value = localStorageService.get('apartment');
+        console.log(value);
+    }
 });
