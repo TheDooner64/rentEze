80d0933e39933356e0a99304602281226691f67f
TheDooner64 <Robert.Muldoon.1@gmail.com>
2016-01-14 19:30:41 -0500

    Add ability to access logged in user in the favorites factory

--- browser/js/common/factories/favorites.factory.js
+++ browser/js/common/factories/favorites.factory.js
@@ -1,12 +1,22 @@
-app.factory('FavoritesFactory', function($http) {
-    var fac = {
-        getAllFavorites: function(userId) {
-            console.log(userId);
-            console.log("In the FavoritesFactory");
-            // return $http.get('/api/users/' + userId + '/favorites/')
-            //     .then(response => response.data)
-        }
-    };
+app.factory('FavoritesFactory', function($http, AuthService) {
+    var FavoritesFactory = {};
 
-    return fac;
+    // NOTE: Need to figure out if there's an easier way to access the current user
+    var loggedInUser = AuthService.getLoggedInUser();
+
+    FavoritesFactory.getAllFavorites = function() {
+        loggedInUser.then(function(user) {
+            // NOTE: Need to add a route to retrieve a user's favorites
+            // return $http.get('/api/users/' + user._id + '/favorites/')
+            // .then(response => response.data)
+        });
+    }
+
+    FavoritesFactory.addFavorite = function() {
+        loggedInUser.then(function(user) {
+            // Need to add route to add a favorite
+        });
+    }
+
+    return FavoritesFactory;
 });
--- browser/js/favorites/favorites.js
+++ browser/js/favorites/favorites.js
@@ -5,9 +5,6 @@ app.config(function($stateProvider) {
         templateUrl: 'js/favorites/favorites.html',
         controller: 'FavoritesCtrl',
         resolve: {
-            getLoggedInUser: function(AuthService) {
-                return AuthService.getLoggedInUser();
-            },
             allFavorites: function(FavoritesFactory) {
                 return FavoritesFactory.getAllFavorites();
             }
@@ -16,15 +13,6 @@ app.config(function($stateProvider) {
 
 });
 
-app.controller('FavoritesCtrl', function($scope, allFavorites, getLoggedInUser) {
-
-    console.dir(getLoggedInUser.toString());
-
-    // getLoggedInUser().then(function(user) {
-    //     $scope.user = user;
-    //     console.log("This should be the user");
-    //     console.log($scope.user);
-    // });
-
+app.controller('FavoritesCtrl', function($scope, FavoritesFactory, allFavorites) {
 
 });
