de8b70aa2e33c5f017fc9c3bb5ae1b9667e429c5
TheDooner64 <Robert.Muldoon.1@gmail.com>
2016-01-15 11:42:12 -0500

    Set up checkout factory, controller, state, html

--- browser/js/checkout/checkout.js
+++ browser/js/checkout/checkout.js
@@ -3,11 +3,18 @@ app.config(function ($stateProvider) {
     $stateProvider.state('checkout', {
         url: '/checkout', // Will need to change to /:userId/checkout
         templateUrl: 'js/checkout/checkout.html',
-        controller: 'CheckoutCtrl'
+        controller: 'CheckoutCtrl',
+        resolve: {
+            loggedInUser: function(AuthService) {
+                return AuthService.getLoggedInUser();
+            }
+        }
     });
 
 });
 
-app.controller('CheckoutCtrl', function ($scope, AuthService, $state) {
+app.controller('CheckoutCtrl', function ($scope, loggedInUser, CheckoutFactory) {
+    console.log(loggedInUser);
 
+    $scope.sendCheckout = CheckoutFactory.sendCheckout;
 });
