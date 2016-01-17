3c114f5d33eedbdc0e4fe7837e193b4ced671693
Nicole <nsiegel2@gmail.com>
2016-01-16 23:19:30 -0500

    edit apartment in database

--- browser/js/admin/apartments/apartments.js
+++ browser/js/admin/apartments/apartments.js
@@ -18,6 +18,8 @@ app.config(function ($stateProvider) {
 
 app.controller('AdminAptCtrl', function ($scope, AuthService, $state, apartments, AdminFactory) {
     $scope.apartments = apartments;
+    $scope.editApt = null;
+    $scope.edit = false;
     $scope.add = false;
 
     // Adds new apartment to db
@@ -31,5 +33,26 @@ app.controller('AdminAptCtrl', function ($scope, AuthService, $state, apartments
         if ($scope.add) $scope.add = false;
         else $scope.add = true;
     };
+
+    // Edits apartment in db
+    $scope.submitEdit = function(updatedApt) {
+        console.log(updatedApt)
+        $scope.edit = false;
+        return AdminFactory.submitEdit(updatedApt);
+    }
+
+    // Puts the apartment info you want to edit on the scope
+    $scope.editApartment = function(apt) {
+        console.log(apt);
+        $scope.editApt = apt;
+        $scope.toggleEdit();
+    };
+
+    // Hides update apartment form
+    $scope.toggleEdit = function() {
+        if ($scope.edit) $scope.edit = false;
+        else $scope.edit = true;
+    };
+
 
 });
