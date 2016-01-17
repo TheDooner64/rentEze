65355771a692e3da987ca67cde9b7169e0692db8
Nicole <nsiegel2@gmail.com>
2016-01-16 23:18:43 -0500

    add new apartment to db

--- browser/js/admin/apartments/apartments.js
+++ browser/js/admin/apartments/apartments.js
@@ -18,5 +18,18 @@ app.config(function ($stateProvider) {
 
 app.controller('AdminAptCtrl', function ($scope, AuthService, $state, apartments, AdminFactory) {
     $scope.apartments = apartments;
+    $scope.add = false;
+
+    // Adds new apartment to db
+    $scope.submitNewApartment = function(aptInfo) {
+        $scope.add = false;
+        return AdminFactory.addApartment(aptInfo);
+    };
+
+    // Hides/shows add new apartment form
+    $scope.toggleAdd = function() {
+        if ($scope.add) $scope.add = false;
+        else $scope.add = true;
+    };
 
 });
