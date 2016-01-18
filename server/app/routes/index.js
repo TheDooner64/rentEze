d2bb4b84ef2c181d19eb56f6dbdd5ce7896f873c
Nicole <nsiegel2@gmail.com>
2016-01-18 11:07:56 -0500

    add orders routes

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -11,6 +11,9 @@ router.use('/apartments', require('./apartments'));
 // /api/reviews
 router.use('/reviews', require('./reviews'));
 
+// /api/orders
+router.use('/orders', require('./orders'));
+
 // Make sure this is after all of
 // the registered routes!
 router.use(function (req, res) {
